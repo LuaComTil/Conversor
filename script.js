7d98eb9e3194465adaa810ef64d896fe947535db
Luã de Freitas Ramos <74731561+LuaComTil@users.noreply.github.com>
2022-04-05 18:30:39 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+//Sistema de conversão
+function convert(){
+
+    //ENCONTRANDO VALOR
+    var convertText = document.getElementById("raw-text").value;
+    //CONVERTENDO PARA MAIÚSCULO
+    convertText = convertText.toUpperCase();
+    //CONVERTENDO O ESPAÇAMENTO
+    convertText=convertText.replace(/\n\n\n/g, '\n'); //Só Deus sabe como isso funciona
+    convertText=convertText.replace(/\n\n/g, '\n');
+    convertText=convertText.replace(/\n/g, '\n\n');
+    //DEVOLVENDO VALOR
+    document.getElementById('converted-text').value = convertText;
+    console.log('texto depois da conversão: ' + convertText);
+}
+//Copiar texto
+function copy() {
+    var copyText = document.getElementById("converted-text");
+    copyText.select();
+    document.execCommand("copy");
+}
